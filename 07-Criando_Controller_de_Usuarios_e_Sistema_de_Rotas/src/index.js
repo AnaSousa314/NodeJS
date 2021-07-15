c704fdba551f3b08f244a67b1844ff567ab93a51
Ana.Sousa <anasousa314@outlook.com>
2021-07-15 16:46:36 -0300

    aula07 criando um controller de usuario e um sistema de rotas

---
+++ 07-Criando_Controller_de_Usuarios_e_Sistema_de_Rotas/src/index.js
@@ -0,0 +1,22 @@
+const http = require("http")
+
+const routes = require('./routes')
+
+const server = http.createServer((req,res)=>{
+  console.log(`Request method: ${req.method} | Endpoint: ${req.url}`);
+
+
+  const route = routes.find((routeObj)=>(
+    routeObj.endpoint === req.url && routeObj.method === req.method
+  ));
+
+  if(route){
+    route.handler(req,res)
+  }else{
+    res.writeHead(404,{'Content-Type':'text/html'});
+    res.end(`Canot ${req.method} ${req.url}`);
+  }
+
+})
+
+server.listen(3000,()=>console.log('Server started at http://localhost:3000'))
